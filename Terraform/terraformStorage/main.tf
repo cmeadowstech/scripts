9e85d7051c05e6dea2b4cadeca966025f5c79bee
Cody Meadows <v-cmeadows@microsoft.com>
2022-12-07 10:02:41 -0600

    encrypt storage

--- Terraform/terraformStorage/main.tf
+++ Terraform/terraformStorage/main.tf
@@ -24,8 +24,10 @@ resource "azurerm_storage_account" "storage" {
   name = "${var.prefix}storage54234524"
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
+  account_kind = "StorageV2"
   account_tier = "Standard"
   account_replication_type = "LRS"
+  infrastructure_encryption_enabled = true
 
   blob_properties {
     versioning_enabled = true
